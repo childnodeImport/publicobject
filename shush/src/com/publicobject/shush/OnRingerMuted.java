bf8fe1044b938b8ce854cc9a1b9f16685cbe5257
limpbizkit <limpbizkit@afa747b4-d56b-11dd-86c2-0f8696b7b6f9>
2011-04-27 07:52:39 +0000

    Fix user-reported conflicts with certain apps
    git-svn-id: http://publicobject.googlecode.com/svn@55 afa747b4-d56b-11dd-86c2-0f8696b7b6f9

--- shush/src/com/publicobject/shush/OnRingerMuted.java
+++ shush/src/com/publicobject/shush/OnRingerMuted.java
@@ -16,6 +16,8 @@
 
 package com.publicobject.shush;
 
+import android.app.ActivityManager;
+import static android.app.ActivityManager.RunningAppProcessInfo.IMPORTANCE_FOREGROUND;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -28,13 +30,49 @@ import static android.media.AudioManager.RINGER_MODE_VIBRATE;
  */
 public class OnRingerMuted extends BroadcastReceiver {
 
+    private static final String[] CONFLICTING_APPS = {
+        // "bt.android.elixir", // widget; always in the foreground
+        "com.idelata.MuteButtonFree",
+        "com.littlephoto",
+        "com.motorola.Camera",
+        "com.noimjosh.profile",
+        "com.urbandroid.sleep",
+        "vStudio.Android.Camera360",
+    };
+
     public void onReceive(Context context, Intent intent) {
         int newRingerMode = intent.getIntExtra(EXTRA_RINGER_MODE, -1);
         if (newRingerMode == RINGER_MODE_SILENT || newRingerMode == RINGER_MODE_VIBRATE) {
+            if (isMutedByApp(context)) {
+                return;
+            }
             context.startActivity(RingerMutedDialog.getIntent(context));
         } else {
             RingerMutedNotification.dismiss(context);
             TurnRingerOn.cancelScheduled(context);
         }
+    }
+
+    /**
+     * Returns true if the current ringer mute intent was likely caused by
+     * another app and not a user action. Some camera apps secretly mute the
+     * ringer to silence the shutter. Other apps manage the ringer on their own
+     * and don't benefit from Shush interference.
+     */
+    private boolean isMutedByApp(Context context) {
+        ActivityManager activityManager
+                = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+        for (ActivityManager.RunningAppProcessInfo p : activityManager.getRunningAppProcesses()) {
+            if (p.importance != IMPORTANCE_FOREGROUND) {
+                continue;
+            }
+            String processName = p.processName;
+            for (String app : CONFLICTING_APPS) {
+                if (processName.equals(app)) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
